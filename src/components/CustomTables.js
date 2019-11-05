6beebc1f4764d417d18ca058b10fb2a25b7da022
Fah11ad <falqurashi11@gmail.com>
2019-11-05 11:13:54 +0300

    version-3

--- src/components/CustomTables.js
+++ src/components/CustomTables.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
-import CustomNavbar from './CustomNavbar'
+import EnglandTable from './EnglandTable'
+import ItalyTable from './ItalyTable'
+import SpainTable from './SpainTable'
+import FranceTable from './FranceTable'
+import GermanyTable from './GermanyTable'
+import { Card, CardDeck, Col, Row, Button, Image } from 'react-bootstrap'
+import '../App.css'
 
 export default class CustomTables extends Component {
     render() {
         return (
             <div>
-                <CustomNavbar/>
                 <h2>Tables</h2>
+                {/* <Row> */}
+                    {/* <Col lg={6} md={6} sm={12}> */}
+                        
+                        <Button className="btn middle" variant="flat" size="xxl">
+                            <Image src={require("../assets/premier-league-logo-preview.png")} style={{width:'150px', height:'150px'}}/>
+                        </Button>
+                        <Button className="btn middle" variant="flat" size="xxl">
+                            <Image src={require("../assets/premier-league-logo-preview.png")} style={{width:'150px', height:'150px'}}/>
+                        </Button>
+                        <Button className="btn middle" variant="flat" size="xxl">
+                            <Image src={require("../assets/premier-league-logo-preview.png")} style={{width:'150px', height:'150px'}}/>
+                        </Button>
+                        <Button className="btn middle" variant="flat" size="xxl">
+                            <Image src={require("../assets/premier-league-logo-preview.png")} style={{width:'150px', height:'150px'}}/>
+                        </Button>
+                        <EnglandTable />
+                        
+                    {/* </Col> */}
+                    {/* <Col lg={6} md={6} sm={12}> */}
+
+                    {/* </Col> */}
+                {/* </Row> */}
             </div>
         )
     }
